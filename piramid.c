05bd95072a1c1dd3a82f8c2c41f0ed24205eddb9
BBCheetah <hoya071878@gmail.com>
2022-01-06 21:13:08 +0900

    first commit

---
+++ piramid.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int a;
+scanf("%d", &a);
+for(a; a != 0; a -= 1){
+        for (int i = 1; i != (a+1); i += 1){
+            printf("*");
+        }
+    printf("\n");
+    }
+}
